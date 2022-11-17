9a39a0c002039431f1b0a9b05d6b6819a10608dc
Andrés Felipe Betancurt Rivera <andbet050197@gmail.com>
2022-11-17 01:13:26 -0500

    Adding more details

--- challenge02/challenge02.py
+++ challenge02/challenge02.py
@@ -5,18 +5,20 @@ def solve():
     with open("encrypted.txt", "r") as content:
         encrypted_code = content.read()
     
-    oration = ''
+    prayer = ''
     for encrypted_word in encrypted_code.split(' '):
         word = ''
         index = 0
         while index < len(encrypted_word):
+            # Only minus ASCII (97-122)
+            # If ASCII start win 9 (97-99) count 2, else (100-122), count 3
             len_ascii = 2 if encrypted_word[index] == '9' else 3
             word += chr(int(encrypted_word[index:index+len_ascii]))
             index += len_ascii
-        oration += "{} ".format(word)
-    oration = oration[:-1] # because last character are " "
+        prayer += "{} ".format(word)
+    prayer = prayer[:-1] # Because last character are " "
 
-    print(oration)
+    print(prayer)
 
 
 if __name__=="__main__":
