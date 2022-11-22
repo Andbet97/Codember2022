154da3cc32de725fcc89cb1d6e66692097347ccc
Andrés Felipe Betancurt Rivera <andbet050197@gmail.com>
2022-11-21 23:21:01 -0500

    Update challenge02.py

--- challenge02/challenge02.py
+++ challenge02/challenge02.py
@@ -1,10 +1,6 @@
 
 
-def solve():
-    encrypted_code = ''
-    with open("encrypted.txt", "r") as content:
-        encrypted_code = content.read()
-    
+def solve(encrypted_code):    
     prayer = ''
     for encrypted_word in encrypted_code.split(' '):
         word = ''
@@ -18,8 +14,49 @@ def solve():
         prayer += "{} ".format(word)
     prayer = prayer[:-1] # Because last character are " "
 
-    print(prayer)
+    return prayer
+
+
+def tests():
+    list_tests = [
+        {
+            'input': "109105100117",
+            'output': 'midu'
+        },
+        {
+            'input': "9911110010110998101114",
+            'output': 'codember'
+        },
+        {
+            'input': "9911110010110998101114 109105100117",
+            'output': 'codember midu'
+        },
+        {
+            'input': "11210897121 116101116114105115",
+            'output': 'play tetris'
+        }
+    ]
+
+    test_result = []
+    
+    for test in list_tests:
+        solution = solve(test['input'])
+        expected = test['output']
+        test_result.append(solution == expected)
+        print('Solution: {}, Expected: {}'.format(solution, expected))
+    
+    return sum(test_result) == len(list_tests)
 
 
 if __name__=="__main__":
-    solve()
+    print('Init tests')
+    passed = tests()
+    print('End tests\n')
+
+    if not passed:
+        print('Try again please')
+    else:
+        encrypted_code = ''
+        with open("encrypted.txt", "r") as content:
+            encrypted_code = content.read()
+        print('Solution: {}'.format(solve(encrypted_code[:-1])))
